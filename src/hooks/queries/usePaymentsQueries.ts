a1391ea81651f8b9343aea7d67c89aca253402ae
Release <support@grovs.io>
2026-09-04 14:45:28 +0300

    Skip the Stripe subscription query on self-hosted builds

--- src/hooks/queries/usePaymentsQueries.ts
+++ src/hooks/queries/usePaymentsQueries.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { IS_SELF_HOSTED } from "@/lib/edition";
 import { queryKeys } from "@/lib/queryKeys";
 import { ApiError } from "@/lib/ApiError";
 import {
@@ -31,7 +32,7 @@ export function useSubscriptionQuery(instanceId: string | undefined) {
         throw error;
       }
     },
-    enabled: !!instanceId,
+    enabled: !!instanceId && !IS_SELF_HOSTED, // no Stripe on self-hosted
   });
 }
 
